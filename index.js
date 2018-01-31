824081f67745b6ccfde24e835f70ff7db7af4660
AliAbuRas <alaburas@mbc.net>
2018-01-31 17:50:15 +0200

    edit port

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 let express = require('express');
 let ejs = require('ejs')
 let app = express();
-
+//
+app.set('port', process.env.PORT || 8010);
+//
 const host="http://localhost:3000/ar/embed/"
-
+//
 app.set('view engine','ejs');
+//
 app.get('/',(req,res)=>{
     console.log('[ QUERY! ] ' +req.query.url)
     res.render('index',{
         "location":host+req.query.url
     });
 })
-
-app.listen(8010);
-console.log('8010');
-
-//aliaburas80@gmail.com
-//ghadeer2910
+//
+app.listen(app.get('port'));
+//
+console.log(app.get('port'));
