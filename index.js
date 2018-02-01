90ad643a0acbdd17f4c5e1ed63e1624e4f4f6811
AliAbuRas <alaburas@mbc.net>
2018-02-01 13:05:43 +0200

    add ifram

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 let express = require('express');
 let ejs = require('ejs')
 let app = express();
+let bodyParser = require("body-parser")
+
 //
 app.set('port', process.env.PORT || 8010);
 //
 const host="http://localhost:3000/ar/embed/"
+
 //
 app.set('view engine','ejs');
 //
-app.get('/',(req,res)=>{
-    console.log('[ QUERY! ] ' +req.query.url)
-    res.render('index',{
-        "location":host+req.query.url
-    });
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: true
+}));
 //
+
+//
+
+app.post('/ifram',(req,res)=>{
+    console.log(req.body);
+    res.render('index',{
+        "url"   :req.body.url,
+        "width" :req.body.width ,
+        "height":req.body.height
+    })
+})
+
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        "url"   :'',
+        "width" :10 ,
+        "height":10
+    })
+    })
+
 app.listen(app.get('port'));
 //
 console.log(app.get('port'));
